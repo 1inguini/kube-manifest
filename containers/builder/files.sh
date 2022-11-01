bae1a8da1901d88cc9d7465c55d341d57daec0d3
1inguini <9647142@gmail.com>
2022-11-01 20:41:05 +0900

    add containers folder

---
+++ containers/builder/files.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# needs moreutls (sponge)
+
+unalias -a
+set -eu
+set -x
+
+# check number of args
+: "$1"
+
+files="$(mktemp)"
+
+# cleanup
+trap "
+rm -rf $files
+" EXIT
+
+paru -Qql filesystem | sed -e '\:/usr/.:d' >> "$files"
+
+paru -S --noconfirm "$@"
+
+paru -Qql "$@" |
+  tee -a "$files" |
+  xargs ldd 2>/dev/null |
+  grep -Po ' /[^ ]*' |
+  sed -e 's/^ //' >> "$files"
+
+# 必要じゃないかも
+printf '%s\n' '/usr/lib/locale/locale-archive' >> "$files"
+printf '%s\n' '/etc/ld.so.cache' >> "$files"
+
+sort <"$files" | uniq | sponge "$files"
+xargs readlink -f <"$files" | sponge -a "$files"
+
+sed -i "$files" -e 's:^/:./:'
+
+mkdir /tmp/files
+sudo tar --ignore-failed-read --no-recursion -C / --verbatim-files-from -T "$files" -cpf - |
+  tar -C /tmp/files/ -xpf -
