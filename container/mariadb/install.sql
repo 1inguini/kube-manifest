8f917dbade3464ab4099afad06485b6e4cc47859
1inguini <9647142@gmail.com>
2022-11-11 20:04:49 +0900

    mariadb client container

--- container/mariadb/install.sql
+++ container/mariadb/install.sql
@@ -1,5 +1,6 @@
 INSTALL SONAME 'auth_ed25519';
-DROP USER IF EXISTS '';
-DROP USER IF EXISTS nonroot;
+DROP USER IF EXISTS '' @localhost;
+DROP USER IF EXISTS '' @buildkitsandbox;
+DROP USER IF EXISTS nonroot @localhost;
 DROP USER IF EXISTS root @localhost;
 GRANT ALL PRIVILEGES ON *.* TO 'root' @'%' IDENTIFIED VIA ed25519 USING PASSWORD('secret');
