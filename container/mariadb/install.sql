dd852234b923a2d8a0f8bcbc5a2c0d06f4dad59d
1inguini <9647142@gmail.com>
2022-11-12 13:39:52 +0900

    wip: builderをEarthfileに

--- container/mariadb/install.sql
+++ container/mariadb/install.sql
@@ -2,5 +2,4 @@ INSTALL SONAME 'auth_ed25519';
 DROP USER IF EXISTS '' @localhost;
 DROP USER IF EXISTS '' @buildkitsandbox;
 DROP USER IF EXISTS nonroot @localhost;
-DROP USER IF EXISTS root @localhost;
-GRANT ALL PRIVILEGES ON *.* TO 'root' @'%' IDENTIFIED VIA ed25519 USING PASSWORD('secret');
+GRANT ALL PRIVILEGES ON *.* TO 'root' @'localhost' IDENTIFIED VIA ed25519 USING PASSWORD('secret');
