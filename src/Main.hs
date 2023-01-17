3e1e870383b7d0f03c52f9aa2bc0524981c06813
1inguini <9647142@gmail.com>
2023-01-17 16:38:57 +0900

    refactor: Util.Shake.Container

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,4 @@
-module Main (
-  main,
-) where
+module Main where
 
 import Manifest (yamls)
 import Util (Yaml, YamlType (..), s)
@@ -17,6 +15,7 @@ import Util.Shake (
   runProc,
   (<:>),
  )
+import Util.Shake.Container (ImageRepo (ImageRepo), addContainerImageRule, image)
 
 import Control.Applicative ((<|>))
 import Control.Exception.Safe (Exception (displayException), finally, throw, throwString)
@@ -199,86 +198,6 @@ import Text.Heredoc (here, str)
 
 -- main :: IO ()
 -- main = generate
-
-newtype ImageRepo = ImageRepo {repo :: String}
-  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
-newtype ImageTag = ImageTag {tag :: String}
-  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
-newtype ImageName = ImageName (ImageRepo, ImageTag) -- (name, tag)
-  deriving (Typeable, Eq, Hashable, Binary, NFData)
-instance Show ImageName where
-  show (ImageName (repo, tag)) = view #repo repo <> ":" <> view #tag tag
-newtype Image = ImageId {id :: ByteString}
-  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
-newtype ImageRule = ImageRule {rule :: ImageName -> Maybe (Action ())}
-type instance RuleResult ImageName = Image
-
-addContainerImageRule :: Rules ()
-addContainerImageRule = addBuiltinRule noLint imageIdentity run
- where
-  imageIdentity :: BuiltinIdentity ImageName Image
-  imageIdentity _ = Just . view #id
-
-  newStore :: ImageName -> Action (Maybe ByteString)
-  newStore name = do
-    (exitCode, stdout) <-
-      readProcessStdout . docker $
-        words "images --no-trunc --quiet" <> [show name]
-    case (exitCode, ByteString.split (fromIntegral $ fromEnum '\n') $ cs stdout) of
-      (ExitSuccess, newStore : _) -> pure $ Just newStore
-      _ -> pure Nothing
-
-  run :: BuiltinRun ImageName Image
-  run key oldStore RunDependenciesChanged = do
-    (_, act) <- getUserRuleOne key (const Nothing) $ \(ImageRule act) -> act key
-    act
-    current <- newStore key
-    case current of
-      Nothing -> throwString $ "action did not create container image:" <:> show key
-      Just current ->
-        pure
-          $ RunResult
-            (if Just current == oldStore then ChangedRecomputeSame else ChangedRecomputeDiff)
-            current
-          $ ImageId current
-  run key oldStore RunDependenciesSame = do
-    current <- newStore key
-    case current of
-      Nothing -> run key oldStore RunDependenciesChanged
-      Just current -> pure $ RunResult ChangedNothing current $ ImageId current
-
-needImages :: [ImageName] -> Action ()
-needImages = void . apply
-
-needImage :: ImageName -> Action ()
-needImage = void . apply1
-
-type ContainerId = String
-type ContainerfileCommand = String
-
-latest :: ImageTag
-latest = ImageTag "latest"
-
-infix 1 `image`
-image :: ImageRepo -> ((?imageName :: ImageName) => Action ()) -> Rules ()
-image repo act = do
-  addTarget $ view #repo repo
-  addTarget . show $ ImageName (repo, latest)
-
-  phonys $ \name -> do
-    colonTag <- List.stripPrefix (view #repo repo) name
-    let tag = case colonTag of
-          "" -> Just latest
-          ':' : tag -> Just $ ImageTag tag
-          _ -> Nothing
-    needImage . ImageName . (,) repo <$> tag
-
-  addUserRule . ImageRule $ \case
-    name@(ImageName (r, _)) | r == repo -> Just $ let ?imageName = name in act
-    _ -> Nothing
-
-addTaggedImageTarget :: ImageName -> Rules ()
-addTaggedImageTarget = addTarget . show
 
 nonrootImage :: (?shakeDir :: FilePath) => Rules ()
 nonrootImage = do
---
+++ src/Util/Shake/Container.hs
@@ -0,0 +1,128 @@
+module Util.Shake.Container (
+  ContainerId,
+  ContainerfileCommand,
+  Image (..),
+  ImageName (..),
+  ImageRepo (..),
+  ImageRule (..),
+  ImageTag (..),
+  addContainerImageRule,
+  addTaggedImageTarget,
+  image,
+  latest,
+  needImage,
+  needImages,
+) where
+
+import Util.Shake (docker, (<:>))
+
+import Control.Exception.Safe (throwString)
+import Control.Monad (void)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+import qualified Data.List as List
+import Data.String.Conversions (cs)
+import Development.Shake (
+  Action,
+  RuleResult,
+  Rules,
+  addTarget,
+  phonys,
+ )
+import Development.Shake.Classes (Binary, Hashable, NFData, Typeable)
+import Development.Shake.Rule (
+  BuiltinIdentity,
+  BuiltinRun,
+  RunChanged (ChangedNothing, ChangedRecomputeDiff, ChangedRecomputeSame),
+  RunMode (RunDependenciesChanged, RunDependenciesSame),
+  RunResult (RunResult),
+  addBuiltinRule,
+  addUserRule,
+  apply,
+  apply1,
+  getUserRuleOne,
+  noLint,
+ )
+import GHC.Generics (Generic)
+import Optics (view)
+import System.Process.Typed (ExitCode (ExitSuccess), readProcessStdout)
+
+newtype ImageRepo = ImageRepo {repo :: String}
+  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
+newtype ImageTag = ImageTag {tag :: String}
+  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
+newtype ImageName = ImageName (ImageRepo, ImageTag) -- (name, tag)
+  deriving (Typeable, Eq, Hashable, Binary, NFData)
+instance Show ImageName where
+  show (ImageName (repo, tag)) = view #repo repo <> ":" <> view #tag tag
+newtype Image = ImageId {id :: ByteString}
+  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
+newtype ImageRule = ImageRule {rule :: ImageName -> Maybe (Action ())}
+type instance RuleResult ImageName = Image
+
+addContainerImageRule :: Rules ()
+addContainerImageRule = addBuiltinRule noLint imageIdentity run
+ where
+  imageIdentity :: BuiltinIdentity ImageName Image
+  imageIdentity _ = Just . view #id
+
+  newStore :: ImageName -> Action (Maybe ByteString)
+  newStore name = do
+    (exitCode, stdout) <-
+      readProcessStdout . docker $
+        words "images --no-trunc --quiet" <> [show name]
+    case (exitCode, ByteString.split (fromIntegral $ fromEnum '\n') $ cs stdout) of
+      (ExitSuccess, newStore : _) -> pure $ Just newStore
+      _ -> pure Nothing
+
+  run :: BuiltinRun ImageName Image
+  run key oldStore RunDependenciesChanged = do
+    (_, act) <- getUserRuleOne key (const Nothing) $ \(ImageRule act) -> act key
+    act
+    current <- newStore key
+    case current of
+      Nothing -> throwString $ "action did not create container image:" <:> show key
+      Just current ->
+        pure
+          $ RunResult
+            (if Just current == oldStore then ChangedRecomputeSame else ChangedRecomputeDiff)
+            current
+          $ ImageId current
+  run key oldStore RunDependenciesSame = do
+    current <- newStore key
+    case current of
+      Nothing -> run key oldStore RunDependenciesChanged
+      Just current -> pure $ RunResult ChangedNothing current $ ImageId current
+
+needImages :: [ImageName] -> Action ()
+needImages = void . apply
+
+needImage :: ImageName -> Action ()
+needImage = void . apply1
+
+type ContainerId = String
+type ContainerfileCommand = String
+
+latest :: ImageTag
+latest = ImageTag "latest"
+
+infix 1 `image`
+image :: ImageRepo -> ((?imageName :: ImageName) => Action ()) -> Rules ()
+image repo act = do
+  addTarget $ view #repo repo
+  addTarget . show $ ImageName (repo, latest)
+
+  phonys $ \name -> do
+    colonTag <- List.stripPrefix (view #repo repo) name
+    let tag = case colonTag of
+          "" -> Just latest
+          ':' : tag -> Just $ ImageTag tag
+          _ -> Nothing
+    needImage . ImageName . (,) repo <$> tag
+
+  addUserRule . ImageRule $ \case
+    name@(ImageName (r, _)) | r == repo -> Just $ let ?imageName = name in act
+    _ -> Nothing
+
+addTaggedImageTarget :: ImageName -> Rules ()
+addTaggedImageTarget = addTarget . show
