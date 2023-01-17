ac28601362dbf1e0234fb79b2d262bc2f863a0ac
1inguini <9647142@gmail.com>
2023-01-17 14:14:01 +0900

    container image rule

--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,10 @@ import Util (Yaml, YamlType (..), s)
 import qualified Util
 
 import Control.Applicative ((<|>))
-import Control.Exception.Safe (finally)
-import Control.Monad (filterM, void)
-import Control.Monad.IO.Class (MonadIO)
+import Control.Exception.Safe (Exception (displayException), finally, throw, throwString)
+import Control.Monad (filterM, void, when)
+import qualified Control.Monad.Catch as Exceptions (MonadCatch (catch), MonadThrow (throwM))
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.State.Strict (execState, get)
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.KeyMap as KeyMap
@@ -18,14 +19,64 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
 import Data.Foldable (foldlM, traverse_)
 import Data.List (isPrefixOf)
+import qualified Data.List as List
 import Data.Maybe (catMaybes, fromMaybe)
 import Data.String (IsString (fromString))
 import Data.String.Conversions (cs)
 import Data.Text (Text)
 import qualified Data.Text as Texs
 import qualified Data.Yaml as Yaml (decodeAllThrow, encode)
-import Development.Shake
+import Development.Shake (
+  Action,
+  FilePattern,
+  Lint (LintBasic),
+  RuleResult,
+  Rules,
+  ShakeOptions (
+    ShakeOptions,
+    shakeColor,
+    shakeFiles,
+    shakeLint,
+    shakeLintInside,
+    shakeLiveFiles,
+    shakeProgress,
+    shakeReport,
+    shakeShare,
+    shakeThreads
+  ),
+  actionCatch,
+  addOracleCache,
+  copyFile',
+  need,
+  parallel,
+  phony,
+  phonys,
+  produces,
+  progressSimple,
+  putError,
+  shakeArgsOptionsWith,
+  shakeOptions,
+  want,
+  writeFile',
+  (%>),
+  (&%>),
+ )
 import qualified Development.Shake as Shake
+import Development.Shake.Classes (Binary, Hashable, NFData, Typeable)
+import Development.Shake.Rule (
+  BuiltinIdentity,
+  BuiltinRun,
+  RunChanged (ChangedNothing, ChangedRecomputeDiff, ChangedRecomputeSame),
+  RunMode (RunDependenciesChanged, RunDependenciesSame),
+  RunResult (RunResult),
+  addBuiltinRule,
+  addUserRule,
+  apply,
+  apply1,
+  getUserRuleOne,
+  noLint,
+ )
+import GHC.Generics (Generic)
 import Optics (modifying, over, preview, view, (%), _head)
 import System.Directory (
   createDirectoryIfMissing,
@@ -42,7 +93,15 @@ import qualified System.Directory as Sys (doesDirectoryExist, doesFileExist)
 import System.FilePath (takeDirectory, (</>))
 import System.Posix (CMode (CMode), setFileCreationMask)
 import System.Posix.Files (setFileCreationMask)
-import System.Process.Typed (proc, readProcessStdout_, readProcess_, runProcess_, setWorkingDir)
+import System.Process.Typed (
+  ExitCode (ExitSuccess),
+  proc,
+  readProcessStdout,
+  readProcessStdout_,
+  readProcess_,
+  runProcess_,
+  setWorkingDir,
+ )
 import Text.Heredoc (here, str)
 
 -- processYaml :: [FilePath] -> Yaml -> IO [FilePath]
@@ -124,6 +183,14 @@ import Text.Heredoc (here, str)
 
 -- main :: IO ()
 -- main = generate
+instance Exceptions.MonadThrow Action where
+  throwM err = do
+    putError $ displayException err
+    liftIO $ Exceptions.throwM err
+instance Exceptions.MonadCatch Action where
+  catch = actionCatch
+instance Exceptions.MonadThrow Rules where
+  throwM = liftIO . Exceptions.throwM
 
 runProc :: MonadIO m => String -> m ()
 runProc = runProcess_ . fromString
@@ -185,8 +252,96 @@ aur opts =
          , "--noconfirm"
          , "--noprovides"
          ]
-
 aurInstall opts = aur $ ["-S"] <> opts
+
+newtype ImageRepo = ImageRepo {repo :: String}
+  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
+newtype ImageTag = ImageTag {tag :: String}
+  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
+newtype ImageName = ImageName (ImageRepo, ImageTag) -- (name, tag)
+  deriving (Typeable, Eq, Hashable, Binary, NFData)
+instance Show ImageName where
+  show (ImageName (repo, tag)) = view #repo repo <> ":" <> view #tag tag
+newtype Image = ImageId {id :: ByteString}
+  deriving (Generic, Show, Typeable, Eq, Hashable, Binary, NFData)
+newtype ImageRule = ImageRule {rule :: ImageName -> Maybe (Action ())}
+type instance RuleResult ImageName = Image
+
+addContainerImageRule :: Rules ()
+addContainerImageRule = addBuiltinRule noLint imageIdentity run
+ where
+  imageIdentity :: BuiltinIdentity ImageName Image
+  imageIdentity _ = Just . view #id
+
+  newStore :: ImageName -> Action (Maybe ByteString)
+  newStore name = do
+    (exitCode, stdout) <-
+      readProcessStdout . docker $
+        words "images --no-trunc --quiet" <> [show name]
+    case (exitCode, ByteString.split (fromIntegral $ fromEnum '\n') $ cs stdout) of
+      (ExitSuccess, newStore : _) -> pure $ Just newStore
+      _ -> pure Nothing
+
+  run :: BuiltinRun ImageName Image
+  run key oldStore RunDependenciesChanged = do
+    (_, act) <- getUserRuleOne key (const Nothing) $ \(ImageRule act) -> act key
+    act
+    current <- newStore key
+    case current of
+      Nothing -> throwString $ "action did not create container image:" <:> show key
+      Just current ->
+        pure
+          $ RunResult
+            (if Just current == oldStore then ChangedRecomputeSame else ChangedRecomputeDiff)
+            current
+          $ ImageId current
+  run key oldStore RunDependenciesSame = do
+    current <- newStore key
+    case current of
+      Nothing -> run key oldStore RunDependenciesChanged
+      Just current -> pure $ RunResult ChangedNothing current $ ImageId current
+
+needImages :: [ImageName] -> Action ()
+needImages = void . apply
+
+needImage :: ImageName -> Action ()
+needImage = void . apply1
+
+type ContainerId = String
+type ContainerfileCommand = String
+
+latest :: ImageTag
+latest = ImageTag "latest"
+
+infix 1 `image`
+infix 1 `imageArbitaryTags`
+
+image :: ImageName -> ((?imageName :: ImageName) => Action ()) -> Rules ()
+image name@(ImageName (repo, tag)) act = do
+  let need = needImage name
+  phony (show name) need
+  when (tag == latest) $ phony (view #repo repo) need
+  addUserRule $
+    ImageRule
+      ( \i ->
+          if name == i
+            then Just $ let ?imageName = name in act
+            else Nothing
+      )
+
+imageArbitaryTags :: ImageRepo -> ((?imageName :: ImageName) => Action ()) -> Rules ()
+imageArbitaryTags repo act = do
+  phonys $ \name -> do
+    colonTag <- List.stripPrefix (view #repo repo) name
+    let tag = case colonTag of
+          "" -> pure latest
+          ':' : tag -> pure $ ImageTag tag
+          _ -> Nothing
+    needImage . ImageName . (,) repo <$> tag
+
+  addUserRule . ImageRule $ \case
+    name@(ImageName (r, _)) | r == repo -> Just $ let ?imageName = name in act
+    _ -> Nothing
 
 nonrootImage :: (?shakeDir :: FilePath) => Rules ()
 nonrootImage = do
@@ -330,6 +485,8 @@ s6PortableUtils = do
 
 rules :: (?projectRoot :: FilePath, ?shakeDir :: FilePath) => Rules ()
 rules = do
+  addContainerImageRule
+
   pacmanSetup
   nonrootImage
   musl
