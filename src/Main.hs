f7e208c98a87b5f4bdb947fe0df8b6e16229af95
1inguini <9647142@gmail.com>
2023-02-22 12:09:03 +0900

    multi-file helm templates|crds

--- src/Main.hs
+++ src/Main.hs
@@ -6,16 +6,16 @@ import Util (Helm, Project, defaultHelm, encodeAll)
 import Control.Exception.Safe (throwString, try)
 import Control.Monad (unless)
 import qualified Data.Aeson as Aeson
-import qualified Data.Aeson.KeyMap as KeyMap
-import Data.Aeson.Optics (_String)
+import Data.Aeson.Optics (key, _String)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
 import Data.Foldable (traverse_)
 import Data.String.Conversions (cs)
 import qualified Data.Yaml as Yaml
-import Optics (Lens', preview, view)
-import System.Directory (createDirectoryIfMissing, removeDirectoryRecursive)
-import System.FilePath (dropFileName, (</>))
+import Optics (Ixed (ix), Lens', preview, view, (%))
+import System.Directory (createDirectoryIfMissing, doesDirectoryExist, listDirectory, removeDirectoryRecursive, removeFile)
+import System.FilePath (dropFileName, (<.>), (</>))
+import Text.Casing (fromHumps, toKebab)
 
 generate :: IO ()
 generate = traverse_ processProject projects
@@ -39,22 +39,50 @@ processProject proj = do
 processHelm :: FilePath -> Helm -> IO ()
 processHelm dir helm = do
   createDirectoryIfMissing True dir
-  let mayWrite :: Eq a => (a -> ByteString) -> Lens' Helm a -> FilePath -> IO ()
-      mayWrite encode field subPath =
-        let val = view field helm
-         in unless (view field defaultHelm == val) $ do
-              let path = dir </> subPath
+  traverse_
+    ( \f ->
+        let path = dir </> f
+         in do
+              isDir <- doesDirectoryExist path
+              if isDir then removeDirectoryRecursive path else removeFile path
+    )
+    =<< listDirectory dir
+  let mayWrite ::
+        forall a. (Eq a, Monoid a) => (a -> ByteString) -> a -> FilePath -> IO ()
+      mayWrite encode val subPath =
+        let path = dir </> subPath
+         in unless ((mempty :: a) == val) $ do
               createDirectoryIfMissing True $ dropFileName path
               ByteString.writeFile path $ encode val
               putStrLn $ "# wrote to: " <> path
-  mayWrite encodeAll #templates "templates/manifests.yaml"
-  mayWrite encodeAll #crds "crds/crds.yaml"
-  mayWrite Yaml.encode #values "values.yaml"
-  mayWrite Yaml.encode #chart "Chart.yaml"
-  mayWrite (cs . Aeson.encode) #valuesSchema "values.schema.json"
-  mayWrite cs #readme "README.md"
-  mayWrite cs #license "LICENSE"
-  mayWrite cs #helmignore ".helmignore"
+      writeField :: (Eq a, Monoid a) => (a -> ByteString) -> Lens' Helm a -> FilePath -> IO ()
+      writeField encode field = mayWrite encode (view field helm)
+      writeTemplates :: [Yaml.Object] -> IO ()
+      writeTemplates = traverse_ $ \yaml -> do
+        name <-
+          maybe (throwString "manifest has no name") (pure . cs) $
+            preview (ix "metadata" % key "name" % _String) yaml
+        kind <-
+          maybe
+            (throwString $ "manifest `" <> name <> "` has no kind")
+            (pure . toKebab . fromHumps . cs)
+            $ preview (ix "kind" % _String) yaml
+        mayWrite Yaml.encode yaml $ "templates" </> name </> kind <.> "yaml"
+      writeCrds :: [Yaml.Object] -> IO ()
+      writeCrds = traverse_ $ \crd -> do
+        name <-
+          maybe (throwString "CRD has no name") (pure . cs) $
+            preview (ix "metadata" % key "name" % _String) crd
+        mayWrite Yaml.encode crd $ "crds" </> name <.> "yaml"
+
+  writeTemplates $ view #templates helm
+  writeCrds $ view #crds helm
+  writeField Yaml.encode #values "values.yaml"
+  writeField Yaml.encode #chart "Chart.yaml"
+  writeField (cs . Aeson.encode) #valuesSchema "values.schema.json"
+  writeField cs #readme "README.md"
+  writeField cs #license "LICENSE"
+  writeField cs #helmignore ".helmignore"
 
 main :: IO ()
 main = do
